Fix normalization to match pvc1 ensemble.

pvc1_loader.py
@@ -175,7 +175,8 @@ class PVC1(torch.utils.data.Dataset):
         # This seems like a random order, but it's to fit with the ordering
         # the standard ordering of conv3d. 
         # https://pytorch.org/docs/stable/generated/torch.nn.Conv3d.html
-        X = (np.stack(imgs, axis=1).astype(np.float32) - 128.0) / 128.0
+        X = (np.stack(imgs, axis=1).astype(np.float32) - 
+             np.array([83, 81, 73], dtype=np.float32).reshape((3, 1, 1))) / 64.0
         mat_file = self.mat_files[tgt['key']]
         
         y = []