d869a3ca4461e888aaa077e6831883aabcbe22c2
Patrick Mineault <patrick.mineault@gmail.com>
2020-12-27 04:32:26 -0500

    Add caching

--- fmri_models.py
+++ fmri_models.py
@@ -1,6 +1,9 @@
 import collections
+import git
 import numpy as np
 import os
+import tables
+from tqdm import tqdm
 
 from modelzoo import gabor_pyramid, separable_net
 from loaders import vim2
@@ -9,6 +12,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+from torchvision.models.resnet import resnet18
 from torchvision.models.vgg import vgg19
 from torchvision.models.video import r3d_18, mc3_18, r2plus1d_18
 
@@ -42,8 +46,8 @@ class Averager(nn.Module):
         nt = 4
         assert X.shape[2] in (10, 20, 40, 80), "X.shape[2] must be 10 x a power of 2"
         stride = X.shape[2] // (nt + 1)  # Always use at most 4 time points
-        delta = (X.shape[2] - (nt - 1) * stride) // 2
-        slc = slice(delta, delta + stride * (nt - 1))
+        delta = (X.shape[2] - nt * stride) // 2
+        slc = slice(delta, delta + stride * nt)
         return X[:, :, slc, :, :].mean(4).mean(3).reshape(
             X.shape[0], 
             X.shape[1], 
@@ -57,31 +61,77 @@ def downsample(movie, width):
                         align_corners=False)
     return movie
 
-def preprocess_data(loader, model, aggregator, activations, metadata, args):
-    Xs = []
-    Ys = []
-    for X, Y in loader:
-        X, Y = X.to(device='cuda'), Y.to(device='cuda')
+def preprocess_data(loader, 
+                    model, 
+                    aggregator, 
+                    activations, 
+                    metadata, 
+                    args):
 
-        with torch.no_grad():
-            X = downsample(X, args.width)
-            if metadata['threed']:
-                result = model(X)
-                fit_layer = activations[args.layer]
-            else:
-                result = model(X.permute(0, 2, 1, 3, 4).reshape(-1, 
-                                                                X.shape[1], 
-                                                                X.shape[3], 
-                                                                X.shape[4]))
-                fit_layer = activations[args.layer]
-                fit_layer = fit_layer.reshape(X.shape[0], X.shape[2], *fit_layer.shape[1:])
-                fit_layer = fit_layer.permute(0, 2, 1, 3, 4)
+    # Check if cache exists for this model.
+    repo = git.Repo(search_parent_directories=True)
+    sha = repo.head.object.hexsha
+    cache_file = f'{args.features}_{args.width}_{args.dataset}_{args.subject}_{loader.dataset.split}_{args.aggregator}_{sha}.h5'
+    cache_file = os.path.join(args.cache_root, cache_file)
 
-            stim = aggregator(fit_layer)
-            Xs.append(stim)
-            Ys.append(Y.squeeze())
+    if not os.path.exists(cache_file):
+        print("Create cache file")
+        h5file = tables.open_file(cache_file, mode="w", title="Cache file")
+        layers = {}
+        outputs = None
+        nrows = len(loader) * loader.batch_size
 
-    return torch.cat(Xs, axis=0), torch.cat(Ys, axis=0)
+        progress_bar = tqdm(total=len(loader), unit='batches', unit_scale=True)
+
+        for i, (X, Y) in enumerate(loader):
+            progress_bar.update(1)
+            X, Y = X.to(device='cuda'), Y.to(device='cuda')
+
+            with torch.no_grad():
+                X = downsample(X, args.width)
+                if metadata['threed']:
+                    result = model(X)
+                    
+                    for layer in activations.keys():
+                        fit_layer = aggregator(activations[layer]).cpu().detach().numpy()
+                        
+                        if outputs is None:
+                            layers[layer] = h5file.create_earray('/', f'layer{layer}', obj=fit_layer, expectedrows=nrows)
+                        else:
+                            layers[layer].append(fit_layer)
+                else:
+                    result = model(X.permute(0, 2, 1, 3, 4).reshape(-1, 
+                                                                    X.shape[1], 
+                                                                    X.shape[3], 
+                                                                    X.shape[4]))
+                    
+                    for layer in activations.keys():
+                        fit_layer = activations[layer]
+                        fit_layer = fit_layer.reshape(X.shape[0], X.shape[2], *fit_layer.shape[1:])
+                        fit_layer = fit_layer.permute(0, 2, 1, 3, 4).cpu().detach().numpy()
+                        fit_layer = aggregator(fit_layer)
+
+                        if outputs is None:
+                            layers[layer] = h5file.create_earray('/', f'layer{layer}', obj=fit_layer, expectedrows=nrows)
+                        else:
+                            layers[layer].append(fit_layer)
+
+                if outputs is None:
+                    outputs = h5file.create_earray('/', 
+                                                f'outputs', 
+                                                obj=Y.cpu().detach().numpy(), 
+                                                expectedrows=nrows)
+                else:
+                    outputs.append(Y.cpu().detach().numpy())
+
+        progress_bar.close()
+        h5file.close()
+    
+    h5file = tables.open_file(cache_file, mode="r")
+    X = torch.tensor(h5file.get_node(f'/layer{args.layer}')[:], device='cuda')
+    Y = torch.tensor(h5file.get_node(f'/outputs')[:], device='cuda').squeeze()
+    h5file.close()
+    return X, Y
 
 
 def get_aggregator(metadata, args):
@@ -149,15 +199,43 @@ def get_feature_model(args):
 
         for i, layer in enumerate(layers):
             assert layer.__repr__().startswith('ReLU')
-            if i == args.layer:
-                layer.register_forward_hook(hook(i))
+            layer.register_forward_hook(hook(i))
         
         metadata = {'sz': 112,
                     'threed': True}
-    elif args.features == 'vgg19':
+    elif args.features in ('vgg19'):
         model = vgg19(pretrained=True)
         layers = [layer for layer in model.features if 
                   layer.__repr__().startswith('ReLU')]
+
+        for i, layer in enumerate(layers):
+            if i == args.layer:
+                layer.register_forward_hook(hook(i))
+
+        metadata = {'sz': 112,
+                    'threed': False}
+    elif args.features in ('resnet18'):
+        model = resnet18(pretrained=True)
+
+        layers = (
+            model.relu,
+            model.layer1[0].relu,
+            model.layer1[0],
+            model.layer1[1].relu,
+            model.layer1[1],
+            model.layer2[0].relu,
+            model.layer2[0],
+            model.layer2[1].relu,
+            model.layer2[1],
+            model.layer3[0].relu,
+            model.layer3[0],
+            model.layer3[1].relu,
+            model.layer3[1],
+            model.layer4[0].relu,
+            model.layer4[0],
+            model.layer4[1].relu,
+            model.layer4[1],
+        )
 
         for i, layer in enumerate(layers):
             if i == args.layer:
--- tests/test_fmri_models.py
+++ tests/test_fmri_models.py
@@ -2,45 +2,135 @@ import sys
 sys.path.append('../')
 import unittest
 
+import tempfile
+import time
+import os
+
 from python_dict_wrapper import wrap
 
 from fmri_models import (get_dataset, 
                          preprocess_data, 
                          get_feature_model,
-                         get_aggregator)
+                         get_aggregator,
+                         Averager)
 
 import torch
 
 class TestFmriModels(unittest.TestCase):
-    def test_s2(self):
-        args = wrap({'subject': 's1',
-                     'batch_size': 10,
-                     'features': 'gaborpyramid3d',
-                     'aggregator': 'average',
-                     'dataset': 'vim2',
-                     'data_root': '../data',
-                     'subset': False,
-                     'layer': 0})
+    def test_averaging(self):
+        avg = Averager()
+        for sz in [10, 20, 40, 80]:
+            X = torch.randn(1, 6, sz, 10, 10)
+            X_ = avg(X)
+            self.assertEqual(X_.shape[1], 24)
+            self.assertEqual(X_.ndim, 2)
 
-        feature_model, activations, metadata = get_feature_model(args)
-        aggregator = get_aggregator(metadata, args)
-        reportset = get_dataset(args, 'report')
+    def test_caching(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            args = wrap({'subject': 's1',
+                        'batch_size': 10,
+                        'features': 'gaborpyramid3d',
+                        'aggregator': 'average',
+                        'dataset': 'vim2',
+                        'data_root': '../data',
+                        'layer': 0,
+                        'width': 112,
+                        'cache_root': tmpdirname})
 
-        reportloader = torch.utils.data.DataLoader(reportset, 
-                                                batch_size=args.batch_size, 
-                                                shuffle=False,
-                                                pin_memory=True
-                                                )
 
-        feature_model.to(device='cuda')
-        X_report, Y_report = preprocess_data(reportloader, 
-                            feature_model, 
-                            aggregator,
-                            activations, 
-                            metadata,
-                            args)
+            feature_model, activations, metadata = get_feature_model(args)
+            aggregator = get_aggregator(metadata, args)
+            reportset = get_dataset(args, 'report')
 
-        print(Y_report.shape)
+            reportloader = torch.utils.data.DataLoader(reportset, 
+                                                    batch_size=args.batch_size, 
+                                                    shuffle=False,
+                                                    pin_memory=True
+                                                    )
+            # Second call should be a cache miss.
+            t = time.time()
+            feature_model.to(device='cuda')
+            X_report, Y_report = preprocess_data(reportloader, 
+                                feature_model, 
+                                aggregator,
+                                activations, 
+                                metadata,
+                                args)
+
+            self.assertEqual(X_report.shape[0], 540)
+            self.assertEqual(X_report.ndim, 2)
+            self.assertEqual(Y_report.shape[0], 540)
+
+            dt = time.time() - t
+
+            # Second call should be a cache hit.
+            t = time.time()
+            feature_model.to(device='cuda')
+            X_report, Y_report = preprocess_data(reportloader, 
+                                feature_model, 
+                                aggregator,
+                                activations, 
+                                metadata,
+                                args)
+
+            dt2 = time.time() - t
+
+            # Should be at least five times faster
+            self.assertGreater(dt, dt2 * 5)
+
+    def test_caching_2d(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            args = wrap({'subject': 's1',
+                        'batch_size': 10,
+                        'features': 'resnet18',
+                        'aggregator': 'average',
+                        'dataset': 'vim2',
+                        'data_root': '../data',
+                        'layer': 0,
+                        'width': 112,
+                        'cache_root': tmpdirname})
+
+
+            feature_model, activations, metadata = get_feature_model(args)
+            aggregator = get_aggregator(metadata, args)
+            reportset = get_dataset(args, 'report')
+
+            reportloader = torch.utils.data.DataLoader(reportset, 
+                                                    batch_size=args.batch_size, 
+                                                    shuffle=False,
+                                                    pin_memory=True
+                                                    )
+            # First call should be a cache miss.
+            t = time.time()
+            feature_model.to(device='cuda')
+            X_report, Y_report = preprocess_data(reportloader, 
+                                feature_model, 
+                                aggregator,
+                                activations, 
+                                metadata,
+                                args)
+
+            self.assertEqual(X_report.shape[0], 540)
+            self.assertEqual(X_report.shape[1], 256)
+            self.assertEqual(X_report.ndim, 2)
+            self.assertEqual(Y_report.shape[0], 540)
+
+            dt = time.time() - t
+
+            # Second call should be a cache hit.
+            t = time.time()
+            feature_model.to(device='cuda')
+            X_report, Y_report = preprocess_data(reportloader, 
+                                feature_model, 
+                                aggregator,
+                                activations, 
+                                metadata,
+                                args)
+
+            dt2 = time.time() - t
+
+            # Should be at least five times faster
+            self.assertGreater(dt, dt2 * 5)
 
 if __name__ == '__main__':
     unittest.main()
