d0101a493822a3448d4b92bb40b9cfb65f36d97d
Patrick Mineault <patrick.mineault@gmail.com>
2021-01-06 17:43:05 -0500

    Logging

--- train_net.py
+++ train_net.py
@@ -44,13 +44,13 @@ def get_dataset(args):
                                     split='train', 
                                     nt=32, 
                                     ntau=9, 
-                                    nframedelay=0)
+                                    nframedelay=2)
 
         tuneset = pvc1.PVC1(os.path.join(args.data_root, 'crcns-ringach-data'), 
                                 split='tune', 
                                 nt=32,
                                 ntau=9,
-                                nframedelay=0)
+                                nframedelay=2)
 
         transform = lambda x: x
         sz = 112
@@ -114,6 +114,13 @@ def log_net(net, layers, writer, n):
                             layer.weight.std(), n)
             writer.add_histogram(f'Weights/{name}/hist', 
                             layer.weight.view(-1), n)
+
+        if hasattr(layer, 'bias') and layer.bias is not None:
+            writer.add_scalar(f'Biases/{name}/mean', 
+                            layer.bias.mean(), n)
+            writer.add_histogram(f'Biases/{name}/hist', 
+                            layer.bias.view(-1), n)
+
 
     for name, param in net.sampler._parameters.items():
         writer.add_scalar(f'Weights/{name}/mean', 
@@ -186,26 +193,35 @@ def get_subnet(args, start_size):
                                    nblocks=args.num_blocks, 
                                    nstartfeats=args.nfeats)
         sz = start_size // 2
+        nfeats = args.nfeats
     if args.submodel.startswith('shallownet'):
         symmetric = 'symmetric' in args.submodel
         subnet = monkeynet.ShallowNet(nstartfeats=args.nfeats,
                                       symmetric=symmetric)
         threed = True
         sz = ((start_size + 1) // 2 + 1) // 2
+        nfeats = args.nfeats
+    if args.submodel.startswith('v1net'):
+        subnet = monkeynet.V1Net()
+        threed = True
+        sz = ((start_size + 1) // 2 + 1) // 2
+        nfeats = args.nfeats
     elif args.submodel == 'gaborpyramid2d':
         subnet = nn.Sequential(
             gabor_pyramid.GaborPyramid(4),
             transforms.Normalize(2.2, 2.2)
         )
         sz = start_size // 2
+        nfeats = args.nfeats
     elif args.submodel == 'gaborpyramid3d':
         subnet = nn.Sequential(
             gabor_pyramid.GaborPyramid3d(4),
             transforms.Normalize(2.2, 2.2)
         )
         threed = True
-        sz = start_size // 2
-    return subnet, threed, sz
+        sz = start_size
+        nfeats = args.nfeats
+    return subnet, threed, sz, nfeats
 
 
 def main(args):
@@ -245,7 +261,7 @@ def main(args):
 
     print("Init models")
     
-    subnet, threed, sz = get_subnet(args, start_sz)
+    subnet, threed, sz, nfeats = get_subnet(args, start_sz)
 
     if args.load_conv1_weights:
         W = np.load(args.load_conv1_weights)
@@ -254,7 +270,7 @@ def main(args):
     subnet.to(device=device)
     net = separable_net.LowRankNet(subnet, 
                                    trainset.total_electrodes, 
-                                   args.nfeats, 
+                                   nfeats, 
                                    sz, 
                                    sz, 
                                    trainset.ntau,
@@ -276,16 +292,30 @@ def main(args):
     if args.single_cell == -1:
         # Make sure to scale things properly because of double counting.
         optimizer = optim.Adam([
-            {'params': net.inner_parameters, 'lr': args.learning_rate / np.sqrt(trainset.total_electrodes)},
-            {'params': net.sampler.parameters(), 'lr': args.learning_rate / np.sqrt(trainset.total_electrodes)},
+            {'params': net.inner_parameters, 'lr': args.learning_rate_outer / np.sqrt(trainset.total_electrodes)},
+            {'params': net.sampler.parameters(), 'lr': args.learning_rate_outer / np.sqrt(trainset.total_electrodes)},
             {'params': subnet.parameters(), 'lr': args.learning_rate},
         ])
+
+        # Use a ramp-up for sensitive components like BN.
+        def ramp_up_one(epoch): 
+            alpha = min(max(epoch - args.warmup / args.batch_size, 0.0) / (args.warmup / args.batch_size), 1.0)
+            return alpha
+
+        # Ramp up the subnet (features) after the exterior components.
+        def ramp_up_two(epoch): 
+            alpha = min(max(epoch - 2 * args.warmup / args.batch_size, 0.0) / (args.warmup / args.batch_size), 1.0)
+            return alpha
+
+        lambdas = [
+            lambda epoch: 1.0,
+            ramp_up_one,
+            ramp_up_two,
+        ]
+        scheduler = optim.lr_scheduler.LambdaLR(optimizer, lambdas)
     else:
         optimizer = optim.Adam(net.parameters(), lr=args.learning_rate)
-    
-    net.requires_grad_(True)
-    net.sampler.requires_grad_(False)
-    subnet.requires_grad_(False)
+        scheduler = None
 
     activations = {}
     def hook(name):
@@ -293,11 +323,16 @@ def main(args):
             activations[name] = o
         return hook_fn
 
-    if hasattr(net.subnet, 'relu'):
-        net.subnet.relu.register_forward_hook(hook('relu'))
+    if hasattr(net.subnet, 'layers'):
+        # Hook the activations
+        for name, layer in net.subnet.layers:
+            layer.register_forward_hook(hook(name))
 
-    ll, m, n = 0, 0, 0
-    print_frequency = 100
+    net.requires_grad_(True)
+    subnet.requires_grad_(True)
+    net.sampler.requires_grad_(True)
+
+    ll, m, n = 0, 0, 0    
     tune_loss = 0.0
 
     Yl = np.nan * torch.ones(100000, trainset.total_electrodes, device=device)
@@ -310,10 +345,6 @@ def main(args):
         for epoch in range(args.num_epochs):  # loop over the dataset multiple times
             for data in trainloader:
                 net.train()
-                if n > args.warmup:
-                    # Release the Kraken!
-                    subnet.requires_grad_(True)
-                    net.sampler.requires_grad_(True)
 
                 # get the inputs; data is a list of [inputs, labels]
                 X, M, w, labels = data
@@ -349,21 +380,26 @@ def main(args):
                 writer.add_scalar('Outputs/mean', output_mean, n)
                 writer.add_scalar('Loss/train', loss.item(), n)
                 
-                if ll % print_frequency == print_frequency - 1:
+                if ll % args.print_frequency == args.print_frequency - 1:
                     log_net(net, layers, writer, n)
-                    print('[%02d, %07d] average train loss: %.3f' % (epoch + 1, n, running_loss / print_frequency ))
+                    print('[%02d, %07d] average train loss: %.3f' % (epoch + 1, n, running_loss / args.print_frequency ))
                     running_loss = 0
                     ll = 0
                     
-                    if 'xception' in args.submodel:
-                        the_max = abs(activations['relu']).max()
-                        the_X_max = abs(X).max()
-                        writer.add_images('Activations/input', 
-                                        .5 * X.squeeze() / the_X_max + .5, 
-                                        n, dataformats='CNHW')
-                        writer.add_images('Activations/relu', 
-                                        .5 * activations['relu'].reshape(-1, 1, net.height_out, net.width_out) / the_max + .5, 
-                                        n, dataformats='NCHW')
+                    if hasattr(net.subnet, 'layers'):
+                        for name, layer in net.subnet.layers:
+                            writer.add_histogram(f'Activations/{name}/hist', 
+                                            activations[name].view(-1), 
+                                            n)
+
+                            writer.add_scalar(f'Activations/{name}/mean', 
+                                            activations[name].mean(), 
+                                            n)
+
+                            writer.add_scalar(f'Activations/{name}/std', 
+                                            activations[name].permute(1, 0, 2, 3, 4).reshape(activations[name].shape[1], -1).std(dim=1).mean(), 
+                                            n)
+                    
                 if ll % 10 == 0:
                     net.eval()
                     try:
@@ -412,10 +448,12 @@ def main(args):
                         tune_loss += loss.item()
                     m += 1
 
-                    if m == print_frequency:
-                        print(f"tune accuracy: {tune_loss /  print_frequency:.3f}")
+                    if m == args.print_frequency:
+                        print(f"tune accuracy: {tune_loss /  args.print_frequency:.3f}")
                         tune_loss = 0
                         m = 0
+
+                scheduler.step()
 
                 n += args.batch_size
                 ll += 1
@@ -454,6 +492,7 @@ if __name__ == "__main__":
     
     parser.add_argument("--submodel", default='xception2d', type=str, help='Sub-model type (currently, either xception2d, gaborpyramid2d, gaborpyramid3d')
     parser.add_argument("--learning_rate", default=5e-3, type=float, help='Learning rate')
+    parser.add_argument("--learning_rate_outer", default=5e-3, type=float, help='Outer learning rate')
     parser.add_argument("--num_epochs", default=20, type=int, help='Number of epochs to train')
     parser.add_argument("--image_size", default=112, type=int, help='Image size')
     parser.add_argument("--batch_size", default=1, type=int, help='Batch size')
@@ -462,6 +501,7 @@ if __name__ == "__main__":
     parser.add_argument("--warmup", default=5000, type=int, help="Number of iterations before unlocking tuning RFs and filters")
     parser.add_argument("--single_cell", default=-1, type=int, help="Fit data to a single cell with this index if true")
     parser.add_argument("--ckpt_frequency", default=2500, type=int, help="Checkpoint frequency")
+    parser.add_argument("--print_frequency", default=100, type=int, help="Print frequency")
 
     parser.add_argument("--no_sample", default=False, help='Whether to use a normal gaussian layer rather than a sampled one', action='store_true')
     parser.add_argument("--no_wandb", default=False, help='Skip using W&B', action='store_true')
