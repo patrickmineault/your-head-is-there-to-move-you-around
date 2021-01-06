8931a569b205df05c92530a1948eb6be5bac7a85
Patrick Mineault <patrick.mineault@gmail.com>
2021-01-06 17:42:41 -0500

    monkeynet tests

--- tests/test_monkeynet.py
+++ tests/test_monkeynet.py
@@ -15,10 +15,16 @@ import torch
 class TestMonkeyNet(unittest.TestCase):
     def test_symmetric(self):
         """Smoke test"""
-        args = wrap({'features': 'MotionNet',
-                     'ckpt_root': '../pretrained'})
         model = monkeynet.ShallowNet(nstartfeats=16, 
                                      symmetric=True)
+        X = torch.randn(1, 3, 80, 112, 112)
+        X_forwarded = model.forward(X)
+        self.assertEqual(X_forwarded.shape[0], 1)
+
+    def test_v1(self):
+        """Smoke test"""
+
+        model = monkeynet.V1Net()
         X = torch.randn(1, 3, 80, 112, 112)
         X_forwarded = model.forward(X)
         self.assertEqual(X_forwarded.shape[0], 1)
