Fix mean and standard deviation

loaders/mt2.py
@@ -196,14 +196,15 @@ class MT2(torch.utils.data.Dataset):
         ims = cache[tgt['images_path']]
         ims = ims[tgt['start_frame']:tgt['end_frame'], :, :].astype(np.float32)
         X = np.stack([ims, ims, ims], axis=0)
-
+f.
         if self.nx is not None:
             X = F.interpolate(torch.tensor(X), 
                              [self.nx, self.ny], 
                              align_corners=False,
                              mode='bilinear')
 
-        X = (X - 40.0) / 40.0
+        # Mean and std for ct0001_arg0466d_128.mat
+        X = (X - 115.0) / 59.0
 
         # Create a mask from the electrode range
         M = np.zeros((self.total_electrodes), dtype=np.bool)

loaders/pvc4.py
@@ -298,6 +298,8 @@ class PVC4(torch.utils.data.Dataset):
         if self.total_electrodes == 1:
             print(f"Loaded cell {self.sequence[-1]['cellid']}")
 
+        self.root = root
+
     def __getitem__(self, idx):
         # Load a single segment of length idx from disk.
         global cache
@@ -305,11 +307,21 @@ class PVC4(torch.utils.data.Dataset):
 
         # The images are natively different sizes, grayscale.
         
+        # Mean and standard deviation vary widely across the image and across 
+        # sequences. We normalize against the mean of means and standard 
+        # deviations across images.
+        if self.root.contains('pvc4'):
+            mm, ss, infill = 54, 43, 20
+            
+        elif self.root.contains('v2'):
+            mm, ss, infill = 73, 47, 73
+
         if tgt['images_path'] not in cache:
             X_ = _loadimfile(tgt['images_path'])
             if X_.shape[1] < tgt['iconside']:
-                X = 20 * np.ones((X_.shape[0], tgt['iconside'], tgt['iconside']), dtype=np.uint8)
-                delta = (X.shape[1] - X_.shape) // 2
+                # The top left background is systematically at value 20.0
+                X = infill * np.ones((X_.shape[0], tgt['iconside'], tgt['iconside']), dtype=np.uint8)
+                delta = (X.shape[1] - X_.shape[1]) // 2
                 assert delta > 0
                 rg = slice(delta, delta + X_.shape[1])
                 X[:, rg, rg] = X_
@@ -327,7 +339,7 @@ class PVC4(torch.utils.data.Dataset):
                              align_corners=False,
                              mode='bilinear')
 
-        X = (X - 40.0) / 40.0
+        X = (X - mm) / ss
 
         # Create a mask from the electrode range
         M = np.zeros((self.total_electrodes), dtype=np.bool)

train_net.py
@@ -1,5 +1,5 @@
 from modelzoo import xception, separable_net, gabor_pyramid, monkeynet
-from loaders import pvc1, pvc2, pvc4
+from loaders import pvc1, pvc2, pvc4, mt2
 
 import argparse
 import datetime
@@ -71,22 +71,41 @@ def get_dataset(args):
         trainset = pvc4.PVC4(os.path.join(args.data_root, 'crcns-pvc4'), 
                                     split='train', 
                                     nt=32, 
-                                    nx=args.image_size + 4,
-                                    ny=args.image_size + 4,
-                                    ntau=9, 
+                                    nx=args.image_size,
+                                    ny=args.image_size,
+                                    ntau=10, 
                                     nframedelay=0,
                                     single_cell=args.single_cell)
 
         tuneset = pvc4.PVC4(os.path.join(args.data_root, 'crcns-pvc4'), 
                                 split='tune', 
                                 nt=32,
-                                nx=args.image_size + 4,
-                                ny=args.image_size + 4,
-                                ntau=9,
+                                nx=args.image_size,
+                                ny=args.image_size,
+                                ntau=10,
                                 nframedelay=0,
                                 single_cell=args.single_cell)
-        transform = transforms.RandomCrop(args.image_size)
-        # transform = lambda x: x
+        transform = lambda x:x
+        sz = args.image_size
+    elif args.dataset == 'mt2':
+        trainset = mt2.MT2(os.path.join(args.data_root, 'crcns-mt2'), 
+                                    split='train', 
+                                    nt=32, 
+                                    nx=args.image_size,
+                                    ny=args.image_size,
+                                    ntau=10, 
+                                    nframedelay=0,
+                                    single_cell=args.single_cell)
+
+        tuneset = mt2.MT2(os.path.join(args.data_root, 'crcns-mt2'), 
+                                split='tune', 
+                                nt=32,
+                                nx=args.image_size,
+                                ny=args.image_size,
+                                ntau=10,
+                                nframedelay=0,
+                                single_cell=args.single_cell)
+        transform = lambda x:x
         sz = args.image_size
     return trainset, tuneset, transform, sz
 
@@ -216,6 +235,14 @@ def get_subnet(args, start_size):
     elif args.submodel == 'gaborpyramid3d':
         subnet = nn.Sequential(
             gabor_pyramid.GaborPyramid3d(4),
+            transforms.Normalize(2.2, 2.2)
+        )
+        threed = True
+        sz = start_size
+        nfeats = args.nfeats
+    elif args.submodel == 'gaborpyramid3d_tiny':
+        subnet = nn.Sequential(
+            gabor_pyramid.GaborPyramid3d(2),
             transforms.Normalize(2.2, 2.2)
         )
         threed = True
@@ -509,7 +536,7 @@ if __name__ == "__main__":
     parser.add_argument("--load_conv1_weights", default='', help="Load conv1 weights in .npy format")
     parser.add_argument("--load_ckpt", default='', help="Load checkpoint")
     parser.add_argument("--dataset", default='pvc4', help='Dataset (currently pvc1, pvc2 or pvc4)')
-    parser.add_argument("--data_root", default='./data', help='Data path')
+    parser.add_argument("--data_root", default='./data_derived', help='Data path')
     parser.add_argument("--output_dir", default='./models', help='Output path for models')
     
     args = parser.parse_args()