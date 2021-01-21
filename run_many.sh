a53f9977774f0cd049c4051deadff3450dd05fea
Patrick Mineault <patrick.mineault@gmail.com>
2021-01-20 21:54:31 -0500

    Run any

--- run_many.sh
+++ run_many.sh
@@ -1,20 +1,160 @@
 #!/bin/bash
+# set -e
 
-models=(gaborpyramid3d r3d_18 mc3_18 r2plus1d_18 vgg19 r3d_18 r3d_18)
-max_layer=(4 16 16 16 15 16 16)
-width=(112 112 112 112 112 56 28)
-for subject in s1 s2 s3;
+# TODO: figure out what to do about SlowFast_Slow and SlowFast_Fast
+
+#models=(gaborpyramid3d ShallowMonkeyNet_pvc1 ShallowMonkeyNet_pvc4 resnet18 MotionNet ShiftNet SlowFast_Slow SlowFast_Fast Slow I3D r3d_18 mc3_18 r2plus1d_18 vgg19)
+#models=(ShiftNet r3d_18 mc3_18 r2plus1d_18 SlowFast_Slow SlowFast_Fast I3D Slow)
+#models=(ShallowMonkeyNet_pvc1)
+#models=(resnet18)
+#for model in "${models[@]}";
+#do
+
+# model="r3d_18"
+# subset="s1"
+
+# python train_convex.py \
+#     --exp_name V1Net \
+#     --dataset "vim2" \
+#     --features "$model" \
+#     --subset "$subset" \
+#     --batch_size 8 \
+#     --slowfast_root ../slowfast \
+#     --aggregator downsample \
+#     --aggregator_sz 8 \
+#     --pca 500 \
+#     --no_save \
+#     --cache_root /mnt/e/cache \
+#     --data_root /mnt/e/data_derived
+
+# #
+# for subset in {0..24};
+# do
+#     python train_convex.py \
+#         --exp_name V1Net \
+#         --dataset "pvc4" \
+#         --features "$model" \
+#         --subset "$subset" \
+#         --batch_size 8 \
+#         --slowfast_root ../slowfast \
+#         --aggregator downsample \
+#         --aggregator_sz 8 \
+#         --pca 500 \
+#         --no_save \
+#         --cache_root /mnt/e/cache \
+#         --data_root /mnt/e/data_derived
+
+#     # Clear cache.
+#     rm /mnt/e/cache/*
+# done
+
+# model=gaborpyramid3d
+
+# for subset in {0..43};
+# do
+#     python train_convex.py \
+#         --exp_name Benchmark \
+#         --dataset "mt2" \
+#         --features "$model" \
+#         --subset "$subset" \
+#         --batch_size 64 \
+#         --slowfast_root ../slowfast \
+#         --aggregator downsample \
+#         --aggregator_sz 8 \
+#         --pca 500 \
+#         --no_save \
+#         --cache_root /mnt/e/cache \
+#         --data_root /mnt/e/data_derived \
+#         --skip_existing
+#     # Clear cache.
+#     rm -f /mnt/e/cache/*
+# done
+
+
+# model=gaborpyramid3d_motionless
+# for subset in {0..120};
+# do
+#     python train_convex.py \
+#         --exp_name Benchmark \
+#         --dataset "v2" \
+#         --features "$model" \
+#         --subset "$subset" \
+#         --batch_size 64 \
+#         --slowfast_root ../slowfast \
+#         --aggregator downsample \
+#         --aggregator_sz 8 \
+#         --pca 500 \
+#         --no_save \
+#         --cache_root /mnt/e/cache \
+#         --data_root /mnt/e/data_derived \
+#         --skip_existing
+#         # Clear cache.
+#     rm -f /mnt/e/cache/*
+# done
+
+# model=gaborpyramid3d
+# for subset in {0..24};
+# do
+#     python train_convex.py \
+#         --exp_name Benchmark \
+#         --dataset "pvc4" \
+#         --features "$model" \
+#         --subset "$subset" \
+#         --batch_size 64 \
+#         --slowfast_root ../slowfast \
+#         --aggregator downsample \
+#         --aggregator_sz 8 \
+#         --pca 500 \
+#         --no_save \
+#         --cache_root /mnt/e/cache \
+#         --data_root /mnt/e/data_derived \
+#         --skip_existing
+
+#     # Clear cache.
+#     rm /mnt/e/cache/*
+# done
+
+# for subset in {0..123};
+# do
+#     python train_convex.py \
+#         --exp_name Benchmark \
+#         --dataset "pvc1" \
+#         --features "$model" \
+#         --subset "$subset" \
+#         --batch_size 64 \
+#         --slowfast_root ../slowfast \
+#         --aggregator downsample \
+#         --aggregator_sz 8 \
+#         --pca 500 \
+#         --no_save \
+#         --cache_root /mnt/e/cache \
+#         --data_root /mnt/e/data_derived \
+#         --skip_existing
+
+#     # Clear cache.
+#     rm /mnt/e/cache/*
+# done
+
+for subset in {0..123};
+do
+    for model in gaborpyramid3d gaborpyramid3d_motionless;
     do
-    for i in "${!models[@]}";
-    do
-        for layer in $(seq 0 1 "${max_layer[$i]}");
-        do
-            python train_fmri_convex.py \
-                --exp_name 20201226 \
-                --layer "$layer" \
-                --features "${models[$i]}" \
-                --subject "$subject" \
-                --width "${width[$i]}"
-        done
+        python train_convex.py \
+            --exp_name Benchmark \
+            --dataset "pvc1" \
+            --features "$model" \
+            --subset "$subset" \
+            --batch_size 64 \
+            --slowfast_root ../slowfast \
+            --aggregator average \
+            --aggregator_sz 8 \
+            --no_save \
+            --cache_root /mnt/e/cache \
+            --data_root /mnt/e/data_derived \
+            --skip_existing \
+            --consolidated
+
+        # Clear cache.
+        rm /mnt/e/cache/*
     done
 done
