7d8fa09db4ad436a65fafd5c90c1a56e932d40d1
Patrick Mineault <patrick.mineault@gmail.com>
2021-01-23 13:47:56 -0500

    Add two options for MT runner

--- run_mt.sh
+++ run_mt.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -e
 
-# TODO: figure out what to do about SlowFast_Slow and SlowFast_Fast
-models=(resnet18 MotionNet ShiftNet Slow I3D mc3_18 r2plus1d_18)
-#models=(gaborpyramid3d r3d_18 ShallowMonkeyNet_pvc1 V1Net)
+# TODO: figure out what to do about SlowFast_Slow and SlowFast_Fast Slow I3D
+# models=(gaborpyramid3d r3d_18 ShallowMonkeyNet_pvc1 V1Net)
+if [ "$1" == "resnet" ]; then
+    models=(MotionNet ShiftNet resnet18 mc3_18 r2plus1d_18)
+elif [ "$1" == "slowfast" ]; then
+    models=(Slow SlowFast_Fast SlowFast_Slow I3D)
+else
+    echo "Unknown type!"
+    exit 1
+fi
+
+
 for model in "${models[@]}";
 do
     for subset in {0..43};
