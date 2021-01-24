7648dabb943d7c3700f45d789f599f67a8c15df6
Patrick Mineault <patrick.mineault@gmail.com>
2021-01-24 17:47:46 -0500

    Downsample further

--- run_mt.sh
+++ run_mt.sh
@@ -8,7 +8,7 @@ if [ "$1" == "resnet" ]; then
     size=8
 elif [ "$1" == "slowfast" ]; then
     models=(Slow SlowFast_Fast SlowFast_Slow I3D)
-    size=6
+    size=4
 else
     echo "Unknown type!"
     exit 1
