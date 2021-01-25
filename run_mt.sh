7920210a11b40fe71defd8e82e1e188500956f0b
Patrick Mineault <patrick.mineault@gmail.com>
2021-01-25 14:30:46 -0500

    More models!

--- run_mt.sh
+++ run_mt.sh
@@ -5,16 +5,18 @@ set -e
 # models=(gaborpyramid3d r3d_18 ShallowMonkeyNet_pvc1 V1Net)
 if [ "$1" == "resnet" ]; then
     # there's something broken about ShiftNet right now
-    models=(mc3_18 r2plus1d_18 resnet18)
+    models=(ShiftNet mc3_18 r2plus1d_18 resnet18)
     size=8
 elif [ "$1" == "slowfast" ]; then
-    models=(I3D)
-    size=4
+    # Slow, , SlowFast_Fast broken
+    # SlowFast_Slow will stay broken for a bit
+    # Slow is a huge model so it's very slow.
+    models=(SlowFast_Fast I3D)
+    size=8
 else
     echo "Unknown type!"
     exit 1
 fi
-
 
 for model in "${models[@]}";
 do
@@ -35,7 +37,8 @@ do
             --pca 500 \
             --no_save \
             --skip_existing \
-            --subsample_layers
+            --subsample_layers \
+            --autotune
 
         # Clear cache.
         rm -f ./cache/*
