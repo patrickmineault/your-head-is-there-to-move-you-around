7ab246fe2ecda94c3ddde413e2aa78fb0a66924b
Patrick Mineault <patrick.mineault@gmail.com>
2021-01-27 14:22:41 -0500

    Cross entropy loss

--- tests/test_airsim.py
+++ tests/test_airsim.py
@@ -19,10 +19,27 @@ class TestAirsimLoader(unittest.TestCase):
         self.assertEqual(tuple(X.shape), (3, 11, 112, 112))
         self.assertEqual(Y.ndim, 1)
         self.assertEqual(Y.size, 5)
-        self.assertLess(X.min(), -1)
-        self.assertGreater(X.min(), -3)
-        self.assertGreater(X.max(), 1)
-        self.assertLess(X.max(),  3)
+        self.assertLess(X.min(), 128)
+        self.assertGreater(X.min(), -1)
+        self.assertGreater(X.max(), 128)
+        self.assertLess(X.max(),  256)
+
+    def test_multiclass(self):
+        loader = airsim.AirSim('/mnt/e/data_derived/airsim', 
+                           split='train',
+                           regression=False
+                           )
+        
+        X, Y = loader[0]
+        self.assertEqual(tuple(X.shape), (3, 11, 112, 112))
+        self.assertEqual(Y.ndim, 1)
+        self.assertEqual(Y.size, 4)
+        self.assertLess(X.min(), 128)
+        self.assertGreater(X.min(), -1)
+        self.assertGreater(X.max(), 128)
+        self.assertLess(X.max(),  256)
+        self.assertEqual(Y.dtype, np.int64)
+        self.assertTrue(np.any(Y > 0))
 
 if __name__ == '__main__':
     unittest.main()
