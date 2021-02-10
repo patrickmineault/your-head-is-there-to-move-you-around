5108d26c7d30279da69e7e273c3ec9bfc02391e9
Patrick Mineault <patrick.mineault@gmail.com>
2021-02-09 23:03:49 -0500

    Patch airsim loader

--- loaders/airsim.py
+++ loaders/airsim.py
@@ -19,47 +19,54 @@ import time
 import torch.nn.functional as F
 import torch.utils.data
 
-cache = {}
+cache = {10: {}, 40: {}}
 
 nclasses = 72  # 5 degree precision in heading discrimination.
+max_speed = 3  # Max 3 m/s movement
+
 
 def to_class(theta):
     theta = theta % (2 * np.pi)
     return int(theta / (2 * np.pi) * nclasses)
 
+
+def to_linear_class(speed, maxspeed):
+    return int(speed / maxspeed * nclasses)
+
+
 class AirSim(torch.utils.data.Dataset):
     """
     Loads a segment from the Airsim flythrough data.
     """
-    def __init__(self, 
-                 root='./airsim',
-                 split='train',
-                 regression=True
-                 ):
 
-        if split not in ('train', 'tune', 'report', 'traintune'):
-            raise NotImplementedError('Split is set to an unknown value')
+    def __init__(self, root="./airsim", split="train", regression=True, nt=10):
+
+        if split not in ("train", "tune", "report", "traintune"):
+            raise NotImplementedError("Split is set to an unknown value")
+
+        assert nt in (10, 40)
 
         self.split = split
         self.root = root
 
         cells = []
-        for item in Path(root).glob('*/*/*.h5'):
+        for item in Path(root).glob("*/*/*.h5"):
             cells.append(item)
 
         cells = sorted(cells)
 
-        splits = {'train': [0, 1, 2, 3, 5, 6, 7, 8],
-            'tune': [4],
-            'report': [9],
-            'traintune': [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        splits = {
+            "train": [0, 1, 2, 3, 5, 6, 7, 8],
+            "tune": [4],
+            "report": [9],
+            "traintune": [0, 1, 2, 3, 4, 5, 6, 7, 8],
         }
         nblocks = 10
 
         sequence = []
         for cell in cells:
-            f = tables.open_file(cell, 'r')
-            labels = f.get_node('/labels')[:]
+            f = tables.open_file(cell, "r")
+            labels = f.get_node("/labels")[:]
             f.close()
 
             for j in range(labels.shape[0]):
@@ -67,37 +74,50 @@ class AirSim(torch.utils.data.Dataset):
                     if regression:
                         # Outputs appropriate for regression
                         sequence.append(
-                            {'images_path': cell,
-                            'labels': np.array([
-                                labels[j]['heading_pitch'], 
-                                np.cos(labels[j]['heading_yaw']), 
-                                np.sin(labels[j]['heading_yaw']), 
-                                np.sin(labels[j]['rotation_pitch']), 
-                                np.sin(labels[j]['rotation_yaw'])
-                                ], dtype=np.float32), 
-                            'idx': j}
+                            {
+                                "images_path": cell,
+                                "labels": np.array(
+                                    [
+                                        labels[j]["heading_pitch"],
+                                        labels[j]["heading_yaw"],
+                                        labels[j]["rotation_pitch"],
+                                        labels[j]["rotation_yaw"],
+                                        labels[j]["speed"],
+                                    ],
+                                    dtype=np.float32,
+                                ),
+                                "idx": j,
+                            }
                         )
                     else:
-                        # Outputs appropriate for multi-class                        
-                        hp = to_class(labels[j]['heading_pitch'])
-                        hy = to_class(labels[j]['heading_yaw'])
-                        rp = to_class(labels[j]['rotation_pitch'])
-                        ry = to_class(labels[j]['rotation_yaw'])
+                        # Outputs appropriate for multi-class
+                        hp = to_class(labels[j]["heading_pitch"])
+                        hy = to_class(labels[j]["heading_yaw"])
+                        rp = to_class(labels[j]["rotation_pitch"])
+                        ry = to_class(labels[j]["rotation_yaw"])
+
+                        # TODO(pmin): make max_speed not hard-coded.
+                        speed = to_linear_class(labels[j]["speed"], max_speed)
 
                         sequence.append(
-                            {'images_path': cell,
-                            'labels': np.array([hp, hy, rp, ry], dtype=np.int64), # Torch requires long ints
-                            'idx': j}
+                            {
+                                "images_path": cell,
+                                "labels": np.array(
+                                    [hp, hy, rp, ry, speed], dtype=np.int64
+                                ),  # Torch requires long ints
+                                "idx": j,
+                            }
                         )
 
         if regression:
             self.noutputs = 5
             self.nclasses = 1
         else:
-            self.noutputs = 4
+            self.noutputs = 5
             self.nclasses = nclasses
 
         self.sequence = sequence
+        self.nt = nt
 
         if len(self.sequence) == 0:
             raise Exception("Didn't find any data")
@@ -107,17 +127,21 @@ class AirSim(torch.utils.data.Dataset):
         global cache
         tgt = self.sequence[idx]
 
-        if tgt['images_path'] not in cache:
-            f = tables.open_file(tgt['images_path'], 'r')
-            X_ = f.get_node('/videos')[:].squeeze()
+        if tgt["images_path"] not in cache:
+            f = tables.open_file(tgt["images_path"], "r")
+            if self.nt == 40:
+                X_ = f.get_node("/videos")[:].squeeze()
+            else:
+                X_ = f.get_node("/short_videos")[:].squeeze()
+
             f.close()
 
-            cache[tgt['images_path']] = X_
+            cache[self.nt][tgt["images_path"]] = X_
 
-        X_ = cache[tgt['images_path']]
-        X_ = X_[tgt['idx'], :].astype(np.float32)
+        X_ = cache[self.nt][tgt["images_path"]]
+        X_ = X_[tgt["idx"], :].astype(np.float32)
         # The images are natively different sizes, grayscale.
-        return (X_.transpose((1, 0, 2, 3)), tgt['labels'])
+        return (X_.transpose((1, 0, 2, 3)), tgt["labels"])
 
     def __len__(self):
         # Returns the length of a dataset
--- tests/test_airsim.py
+++ tests/test_airsim.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('../')
+
+sys.path.append("../")
 
 from loaders import airsim
 import numpy as np
@@ -9,37 +10,54 @@ import unittest
 import torch
 from pprint import pprint
 
+
 class TestAirsimLoader(unittest.TestCase):
     def test_train(self):
-        loader = airsim.AirSim('/mnt/e/data_derived/airsim', 
-                           split='train',
-                           )
-        
+        loader = airsim.AirSim(
+            "/mnt/e/data_derived/airsim/batch2",
+            split="train",
+        )
+
         X, Y = loader[0]
-        self.assertEqual(tuple(X.shape), (3, 11, 112, 112))
+        self.assertEqual(tuple(X.shape), (3, 10, 112, 112))
         self.assertEqual(Y.ndim, 1)
         self.assertEqual(Y.size, 5)
         self.assertLess(X.min(), 128)
         self.assertGreater(X.min(), -1)
         self.assertGreater(X.max(), 128)
-        self.assertLess(X.max(),  256)
+        self.assertLess(X.max(), 256)
 
-    def test_multiclass(self):
-        loader = airsim.AirSim('/mnt/e/data_derived/airsim', 
-                           split='train',
-                           regression=False
-                           )
-        
+    def test_big(self):
+        loader = airsim.AirSim(
+            "/mnt/e/data_derived/airsim/batch2", split="train", nt=40
+        )
+
         X, Y = loader[0]
-        self.assertEqual(tuple(X.shape), (3, 11, 112, 112))
+        self.assertEqual(tuple(X.shape), (3, 40, 112, 112))
         self.assertEqual(Y.ndim, 1)
-        self.assertEqual(Y.size, 4)
+        self.assertEqual(Y.size, 5)
         self.assertLess(X.min(), 128)
         self.assertGreater(X.min(), -1)
         self.assertGreater(X.max(), 128)
-        self.assertLess(X.max(),  256)
+        self.assertLess(X.max(), 256)
+
+    def test_multiclass(self):
+        loader = airsim.AirSim(
+            "/mnt/e/data_derived/airsim/batch2", split="train", regression=False
+        )
+
+        X, Y = loader[0]
+        self.assertEqual(tuple(X.shape), (3, 10, 112, 112))
+        self.assertEqual(Y.ndim, 1)
+        self.assertEqual(Y.size, 5)
+        self.assertEqual(Y.dtype, np.int64)
+        self.assertLess(X.min(), 128)
+        self.assertGreater(X.min(), -1)
+        self.assertGreater(X.max(), 128)
+        self.assertLess(X.max(), 256)
         self.assertEqual(Y.dtype, np.int64)
         self.assertTrue(np.any(Y > 0))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
