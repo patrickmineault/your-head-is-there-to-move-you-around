9149fdff23f6d524fadb486bf39bcc2fea8d9c4c
Patrick Mineault <patrick.mineault@gmail.com>
2021-03-05 13:48:58 -0500

    Add point decoder

--- modelzoo/decoder.py
+++ modelzoo/decoder.py
@@ -1,22 +1,17 @@
 from torch import nn
 
+
 class Average(nn.Module):
-    def __init__(self, 
-                 subnet, 
-                 noutputs,
-                 nclasses,
-                 nfeats,
-                 threed
-                 ):
+    def __init__(self, subnet, noutputs, nclasses, nfeats, threed):
         super().__init__()
         self.subnet = subnet
         self.noutputs = noutputs
         self.nclasses = nclasses
         self.nfeats = nfeats
         self.threed = threed
-        self.fully_connected = nn.Linear(self.nfeats, 
-                                         self.noutputs * self.nclasses, 
-                                         bias=True)
+        self.fully_connected = nn.Linear(
+            self.nfeats, self.noutputs * self.nclasses, bias=True
+        )
 
     def forward(self, X):
         X = self.subnet(X)
@@ -29,28 +24,56 @@ class Average(nn.Module):
             Y = Y.reshape(Y.shape[0], self.nclasses, self.noutputs)
         return Y
 
-class Center(nn.Module):
-    def __init__(self, 
-                 subnet, 
-                 noutputs,
-                 nclasses,
-                 nfeats,
-                 threed
-                 ):
+
+class Point(nn.Module):
+    def __init__(self, subnet, noutputs, nclasses, nfeats, threed):
         super().__init__()
         self.subnet = subnet
         self.noutputs = noutputs
         self.nclasses = nclasses
         self.nfeats = nfeats
         self.threed = threed
-        self.fully_connected = nn.Linear(self.nfeats, 
-                                         self.noutputs * self.nclasses, 
-                                         bias=True)
+        self.fully_connected = nn.Linear(
+            self.nfeats, self.noutputs * self.nclasses, bias=True
+        )
 
     def forward(self, X):
         X = self.subnet(X)
-        ct, cy, cx = X.shape[2] // 2, X.shape[3] // 2, X.shape[4] // 2, 
+        ct, cy, cx = (
+            X.shape[2] // 2,
+            X.shape[3] // 2,
+            X.shape[4] // 2,
+        )
         Xs = X[:, :, ct, cy, cx]
+
+        assert Xs.ndim == 2
+        assert Xs.shape[1] == self.nfeats
+        Y = self.fully_connected.forward(Xs)
+        if self.nclasses > 1:
+            Y = Y.reshape(Y.shape[0], self.nclasses, self.noutputs)
+        return Y
+
+
+class Center(nn.Module):
+    def __init__(self, subnet, noutputs, nclasses, nfeats, threed):
+        super().__init__()
+        self.subnet = subnet
+        self.noutputs = noutputs
+        self.nclasses = nclasses
+        self.nfeats = nfeats
+        self.threed = threed
+        self.fully_connected = nn.Linear(
+            self.nfeats, self.noutputs * self.nclasses, bias=True
+        )
+
+    def forward(self, X):
+        X = self.subnet(X)
+        ct, cy, cx = (
+            X.shape[2] // 2,
+            slice(X.shape[3] // 4, int(3 * X.shape[3] / 4)),
+            slice(X.shape[4] // 4, int(3 * X.shape[4] / 4)),
+        )
+        Xs = X[:, :, ct, cy, cx].mean(3).mean(2)
 
         assert Xs.ndim == 2
         assert Xs.shape[1] == self.nfeats
