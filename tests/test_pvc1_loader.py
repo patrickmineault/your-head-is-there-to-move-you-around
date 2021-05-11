ca0c7bdb839584436076ae3ef7cc8291f640548d
Patrick Mineault <patrick.mineault@gmail.com>
2021-05-11 02:30:40 -0400

    Add test for repeats

--- tests/test_pvc1_loader.py
+++ tests/test_pvc1_loader.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('../')
+
+sys.path.append("../")
 
 from loaders import pvc1
 import tempfile
@@ -8,62 +9,85 @@ import unittest
 import torch
 from pprint import pprint
 
+
 class TestPvc1Loader(unittest.TestCase):
+    def test_repeats(self):
+        loader = pvc1.PVC1(
+            "/mnt/e/data_derived/crcns-ringach-data/",
+            nt=9,
+            ntau=7,
+            nframedelay=0,
+        )
+        self.assertEqual(loader.total_electrodes, 127)
+
+        loader = pvc1.PVC1(
+            "/mnt/e/data_derived/crcns-ringach-data/",
+            nt=9,
+            ntau=7,
+            nframedelay=0,
+            repeats=True,
+        )
+        self.assertEqual(loader.total_electrodes, 23)
+
+        loader = pvc1.PVC1(
+            "/mnt/e/data_derived/crcns-ringach-data/",
+            nt=9,
+            ntau=7,
+            nframedelay=0,
+            repeats=True,
+            single_cell=0,
+        )
+        self.assertEqual(loader.total_electrodes, 1)
+
+    @unittest.skip("Irrelevant")
     def test_virtual(self):
-        loader = pvc1.PVC1('/mnt/e/data_derived/crcns-ringach-data/',
-                    nt=9,
-                    ntau=7,
-                    nframedelay=0,
-                    virtual='rot')
+        loader = pvc1.PVC1(
+            "/mnt/e/data_derived/crcns-ringach-data/",
+            nt=9,
+            ntau=7,
+            nframedelay=0,
+            virtual="rot",
+        )
         self.assertEqual(loader.total_electrodes, 508)
         X0, _, _, _ = loader[0]
         X1, _, _, _ = loader[len(loader) // 2]
         self.assertEqual(X0[0, 0, 0, 0], X1[0, 0, -1, -1])
 
+    @unittest.skip("Irrelevant")
     def test_delays(self):
-        loader = pvc1.PVC1('../data/crcns-ringach-data/',
-                    nt=9,
-                    ntau=7,
-                    nframedelay=0)
+        loader = pvc1.PVC1("../data/crcns-ringach-data/", nt=9, ntau=7, nframedelay=0)
         s0 = loader.sequence[0]
         # according to the docs, frame k runs from kT to (k+1)T,
         # where T = 1/30. Hence the end of the last frame should coincide
         # with the start of the response.
-        self.assertEqual(s0['end_frame'], s0['spike_frames'][-2])
+        self.assertEqual(s0["end_frame"], s0["spike_frames"][-2])
 
-        loader = pvc1.PVC1('../data/crcns-ringach-data/',
-                           nt=1,
-                           ntau=7,
-                           nframedelay=0)
+        loader = pvc1.PVC1("../data/crcns-ringach-data/", nt=1, ntau=7, nframedelay=0)
 
         s0 = loader.sequence[0]
-        self.assertEqual(s0['end_frame'], s0['spike_frames'][-2])
+        self.assertEqual(s0["end_frame"], s0["spike_frames"][-2])
 
     @unittest.skip("Slow")
     def test_batching(self):
-        loader = pvc1.PVC1('../data/crcns-ringach-data/',
-                                  ntau=7)
+        loader = pvc1.PVC1("../data/crcns-ringach-data/", ntau=7)
 
-        trainloader = torch.utils.data.DataLoader(loader, 
-                                                  batch_size=8, 
-                                                  shuffle=True)
-        
+        trainloader = torch.utils.data.DataLoader(loader, batch_size=8, shuffle=True)
+
         data = next(iter(trainloader))
         (X, rg, w, y) = data
 
         self.assertEqual(X.shape[0], rg.shape[0])
         self.assertEqual(rg.shape[0], y.shape[0])
         self.assertEqual(rg.shape[1], y.shape[1])
-        
+
         self.assertEqual(y.ndim, 3)
         self.assertEqual(y.shape[2], loader.nt)
         self.assertEqual(y.shape[0], 8)
 
     @unittest.skip("Slow")
     def test_sequential(self):
-        loader = pvc1.PVC1('../data/crcns-ringach-data/',
-                                  ntau=7)
-        
+        loader = pvc1.PVC1("../data/crcns-ringach-data/", ntau=7)
+
         (X, rg, w, y) = loader[0]
 
         self.assertEqual(X.ndim, 4)
@@ -77,8 +101,10 @@ class TestPvc1Loader(unittest.TestCase):
         self.assertEqual(y.shape[1], loader.nt)
 
         # Same stimulus, different electrode.
-        self.assertEqual(loader.sequence[0]['spike_frames'][-1], 
-                         loader.sequence[1]['spike_frames'][-1])
+        self.assertEqual(
+            loader.sequence[0]["spike_frames"][-1],
+            loader.sequence[1]["spike_frames"][-1],
+        )
 
         # self.assertEqual(loader.sequence[0]['end_frame'] - loader.ntau + 1,
         #                  loader.sequence[1]['start_frame'])
@@ -107,5 +133,5 @@ class TestPvc1Loader(unittest.TestCase):
         self.assertLess(time.time() - t0, 2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
