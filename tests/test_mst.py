60c197991ea0abca4f99578100e42064b0325c9c
Patrick Mineault <patrick.mineault@gmail.com>
2021-05-11 02:31:04 -0400

    Test padding

--- tests/test_mst.py
+++ tests/test_mst.py
@@ -11,6 +11,7 @@ import torch
 from pprint import pprint
 
 ROOT = "/mnt/e/data_derived/packlab-mst"
+ROOT_DORSAL = "/mnt/e/data_derived/packlab-dorsal"
 
 
 class TestMstLoader(unittest.TestCase):
@@ -31,9 +32,41 @@ class TestMstLoader(unittest.TestCase):
         self.assertEqual(y.ndim, 2)
         self.assertEqual(y.shape[1], 1)
 
+    def test_pad(self):
+        loader = mst.MST(
+            ROOT,
+            nt=1,
+            ntau=12,
+            split="train",
+        )
+
+        self.assertEqual(
+            len({x["cellnum"]: 1 for x in loader.sequence}), loader.total_electrodes
+        )
+
+        X, m, W, y = loader[0]
+        self.assertEqual(X.shape, (3, 12, 112, 112))
+        self.assertEqual(m.shape, W.shape)
+        self.assertEqual(y.ndim, 2)
+        self.assertEqual(y.shape[1], 1)
+
     def test_singlecell(self):
         loader = mst.MST(
             ROOT,
+            nt=1,
+            split="traintune",
+            single_cell=0,
+        )
+
+        X, m, W, y = loader[0]
+        self.assertEqual(X.shape, (3, 10, 112, 112))
+        self.assertEqual(m.shape, W.shape)
+        self.assertEqual(y.ndim, 2)
+        self.assertEqual(y.shape[1], 1)
+
+    def test_singlecell_dorsal(self):
+        loader = mst.MST(
+            ROOT_DORSAL,
             nt=1,
             split="traintune",
             single_cell=0,
