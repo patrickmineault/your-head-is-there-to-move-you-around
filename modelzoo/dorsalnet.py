a1dc81c2cf3246f1546e572fe7fba220a5375742
Patrick Mineault <patrick.mineault@gmail.com>
2021-05-31 17:53:03 -0400

    Switch to dot notation

--- modelzoo/dorsalnet.py
+++ modelzoo/dorsalnet.py
@@ -4,7 +4,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-from resblocks import ResBlock, BottleneckTransform
+from .resblocks import ResBlock, BottleneckTransform
 
 
 class SymmetricConv3d(nn.Module):
