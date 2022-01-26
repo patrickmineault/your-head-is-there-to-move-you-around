65c96d7309794e49447e6e4c73db559eb0588cb1
Patrick Mineault <patrick.mineault@gmail.com>
2022-01-25 23:22:45 -0500

    Show results

--- notebooks/Compare results_physiology.py
+++ notebooks/Compare results_physiology.py
@@ -14,7 +14,7 @@
 # ---
 
 # %% [markdown]
-# # Core table of results
+# # Core results
 
 # %%
 import collections
@@ -280,340 +280,11 @@ for dataset in datasets:
     nbetter = (df_.dorsalnet > df_.motionnet).sum()
     ntotal = df_.shape[0]
     print(f"{dataset}, cells better {nbetter}/{ntotal}")
-    
 
-# %% [markdown]
-# # Bump plot
-# Show the alignment of each area to dorsalnet layers.
-
-# %%
-df.dataset.unique()
-
-# %%
-dmap = {'mst': 'mst', 'mt1': 'mt', 'mt2': 'mt', 'pvc1': 'v1', 'pvc4': 'v1'}
-df['area'] = df.dataset.map(lambda x: dmap[x])
-df_ = df.query('features == "dorsalnet"')
-
-df_.groupby(['area', 'layer']).rnorm.mean()
-
-def bs(df):
-    df = df.copy()
-    idx = np.random.randint(0, df.shape[0], df.shape[0])
-    df_ = df.iloc[idx]
-    the_max = df_.groupby(['area', 'layer']).rnorm.mean().groupby('area').max()
-    df_ = df_.groupby(['area', 'layer']).rnorm.mean() / the_max
-    return df_
-
-dmap = {'pvc1': 'v1', 'pvc4': 'v1', 'mt1': 'mt', 'mt2': 'mt', 'mst': 'mst'}
-
-df['area'] = df.dataset.map(lambda x: dmap[x])
-df_ = df.query('features == "dorsalnet"')
-df_ = df_.sort_values('area', ascending=True)
-
-df_['rnorm_max'] = df_.rnorm
-A = np.array([bs(df_).values for x in range(500)])
-df__ = pd.DataFrame(A.T)
-df__.index = bs(df_).index
-plt.figure(figsize=(4, 3))
-plt.figure(figsize=(4, 3))
-
-for area in ['v1', 'mt', 'mst']:
-    vals = np.quantile(df__.loc[area], [.05, .95, .5], axis=1).T
-    plt.fill_between(df__.loc[area].index, vals[:, 0], vals[:, 1], alpha=.2)
-    plt.plot(df__.loc[area].index, vals[:, 2])
-sns.despine()
-plt.xlabel('layer')
-plt.ylabel('R relative to maximum for area')
-plt.yticks([0, .2, .4, .6, .8, 1.0])
-
-cols = sns.color_palette(as_cmap=True)
-for i, area in enumerate(['v1', 'mt', 'mst']):
-    df_ = df.query(f'report_corr > .01 and features == "dorsalnet" and area == "{area}"')
-    df_['dataset_subset'] = df_.dataset + '_' + df_.subset.astype(np.str)
-    v = df_.pivot('dataset_subset', 'layer', 'rnorm').values
-    
-    vals = []
-    for n in range(10000):
-        v_ = v[np.random.randint(low=0, high=v.shape[0], size=v.shape[0]), :].argmax(axis=1)#mean(axis=0)
-        #cs = CubicSpline(np.arange(6), v_)
-        #xs = np.linspace(0, 5)
-        vals.append(v_.mean())
-        
-    print(np.mean(vals))
-    print(np.std(vals))
-    
-    q = np.quantile(vals, [.025, .975])
-    plt.plot(q, (.2 + .1*i) * np.array([1, 1]), color=g.lines[2-i].get_color())
-    plt.text((q[0] + q[1])/2, .225 + .1*i, area, {'ha': 'center'})
-    print(np.quantile(vals, [.025, .5, .975]))
-    
-plt.ylim([0, 1.05])
-plt.savefig('/home/pmin/paper-assets/figure-2-layer-alignment.pdf', bbox_inches='tight')
-
-# %%
-dmap = {'pvc1': 'v1', 'pvc4': 'v1', 'mt1': 'mt', 'mt2': 'mt', 'mst': 'mst'}
-
-df['area'] = df.dataset.map(lambda x: dmap[x])
-df_ = df.query('features == "dorsalnet"')
-df_ = df_.sort_values('area', ascending=True)
-
-df_['rnorm_max'] = df_.rnorm
-df_.loc[df_.area == 'v1', 'rnorm_max'] /= .3618
-df_.loc[df_.area == 'mt', 'rnorm_max'] /= .2954
-df_.loc[df_.area == 'mst', 'rnorm_max'] /= .4540
-
-plt.figure(figsize=(4, 3))
-g = sns.lineplot(x='layer', 
-                y='rnorm_max', 
-                hue='area',
-                palette="Set2",
-                data=df_, legend=False, ax=plt.gca())
-sns.despine()
-#plt.legend(loc='upper right', fancybox=None, frameon=False, bbox_to_anchor=(0.,.3, 1.0, .105))
-plt.ylabel('R relative to maximum for area')
-plt.yticks([0, .2, .4, .6, .8, 1.0])
-cols = sns.color_palette(as_cmap=True)
-for i, area in enumerate(['v1', 'mt', 'mst']):
-    df_ = df.query(f'report_corr > .01 and features == "dorsalnet" and area == "{area}"')
-    df_['dataset_subset'] = df_.dataset + '_' + df_.subset.astype(np.str)
-    v = df_.pivot('dataset_subset', 'layer', 'rnorm').values
-    
-    vals = []
-    for n in range(10000):
-        v_ = v[np.random.randint(low=0, high=v.shape[0], size=v.shape[0]), :].argmax(axis=1)#mean(axis=0)
-        #cs = CubicSpline(np.arange(6), v_)
-        #xs = np.linspace(0, 5)
-        vals.append(v_.mean())
-        
-    print(np.mean(vals))
-    print(np.std(vals))
-    
-    q = np.quantile(vals, [.025, .975])
-    plt.plot(q, (.2 + .1*i) * np.array([1, 1]), color=g.lines[2-i].get_color())
-    plt.text((q[0] + q[1])/2, .225 + .1*i, area, {'ha': 'center'})
-    print(np.quantile(vals, [.025, .5, .975]))
-    
-plt.ylim([0, 1.2])
-
-#plt.savefig('/home/pmin/paper-assets/figure-2-layer-alignment.pdf', bbox_inches='tight')
-
-# %% [markdown]
-# # Sparse regression
-
-# %%
-api = wandb.Api()
-
-runs = api.runs("pmin/train_fmri_convex.py", 
-                {"$and": 
-                 [
-                     {"config.dataset": {"$in": ["pvc1-repeats", "pvc4", "mt1_norm_neutralbg", "mt2", "mst_norm_neutralbg"]}},
-                     {"config.aggregator": "downsample"},
-                     {"config.pca": -1},
-                     {"config.method": "boosting"},
-                     {"state": "finished"},
-                ]
-                }
-               )
-
-archives = {}
-
-
-print("Found %i" % len(runs))
-for run in tqdm(runs):
-    if run.config['features'] == 'SlowFast':
-        if run.config['layer'] > 16:
-            run.config['features'] = 'SlowFast_Fast'
-            run.config['layer'] = run.config['layer'] - 17
-        else:
-            run.config['features'] = 'SlowFast_Slow'
-    
-    unique_name = f"{run.config['features']}_layer{int(run.config['layer']):02}_{run.config['dataset']}_{(run.config['subset'])}"
-    if unique_name in archives:
-        continue
-        
-    if 'corrs_report' in run.summary:
-        maxr2_ = [1]
-        if run.config['dataset'] in maxr2.keys():
-            maxr2_ = [maxr2[run.config['dataset']][int(run.summary['subset'])]]
-            
-        archives[unique_name] = {'corrs_report': [run.summary['corrs_report']],
-                                 'maxr2': maxr2_,
-                                 'config': run.config,
-                                }
-
-# %%
-
-results = []
-for k, v in archives.items():
-    fam = families[v['config']['features']]
-    try:
-        layer_num = int(v['config']['layer_name'][-2:])
-    except (KeyError, ValueError):
-        layer_num = int(v['config']['layer'])
-
-    friendly_name = friendly_names[v['config']['features']]
-    if friendly_name not in whitelist.keys() or layer_num not in whitelist[friendly_name]:
-        continue
-    
-    if v['corrs_report'][0] is None:
-        # This happens when boosting chooses no 
-        v['corrs_report'][0] = 0.0
-        
-    results += [{'key': k, 
-                 'family': fam,
-                 'dataset': dataset_names[v['config']['dataset']],
-                 'subset': int(v['config']['subset']) + i*100,
-                 'features': friendly_name,
-                 'layer': layer_num,
-                 'features_layer': f"{friendly_name}_{layer_num:02}",
-                 'report_corr': x,
-                 'max_r2': np.sqrt(max([m, .1])),
-                 'rnorm': x / np.sqrt(max([m, .1]))} for i, (x, m) in enumerate(zip(v['corrs_report'], v['maxr2']))]
-
-df = pd.DataFrame(results)
-df = df.sort_values('key')
-nums = df.groupby(['dataset', 'features', 'layer']).subset.count().reset_index()
-df = df.merge(nums, left_on=('dataset', 'features', 'layer'), right_on=('dataset', 'features', 'layer'), suffixes=('', '_overall'))
-df = df[df.subset_overall.isin([23, 25, 36, 44, 84])]
-#df.query('dataset == "mst"').groupby('features_layer').mean()
-#df.query('features == "dorsalnet"')
-nums
-
-# %%
-#df_ = df.groupby(['dataset', 'features', 'layer']).rnorm.agg(['mean', 'sem', 'count'])
-#df_.reset_index().groupby(['dataset', 'features'])['mean'].max()
-datasets = df.dataset.unique()
-perf = []
-for dataset in datasets:
-    df_ = df.query(f'dataset == "{dataset}"')
-    df_m = df_.pivot_table('rnorm', 'features', 'layer')
-    df_s = df_.pivot_table('rnorm', 'features', 'layer', aggfunc='sem')
-
-    m = np.diag(df_m.loc[df_m.index, df_m.idxmax(axis=1)].values)
-    s = np.diag(df_s.loc[df_s.index, df_m.idxmax(axis=1)].values)
-    
-    series = df_.groupby(['features_layer']).mean().report_corr
-    best = series.index[series.argmax()]
-    
-    df_ = pd.merge(df_, df_.query(f'features_layer == "{best}"')[['subset', 'rnorm']], left_on='subset', right_on='subset', suffixes=('', '_best'))
-    df_['rnorm_delta'] = df_['rnorm'] - df_['rnorm_best']
-    df_m_delta = df_.pivot_table('rnorm_delta', 'features', 'layer')
-    df_s_delta = df_.pivot_table('rnorm_delta', 'features', 'layer', aggfunc='sem') + 1e-6
-    df_ = (df_m_delta / df_s_delta).max(axis=1)
-
-    perf += [{'dataset': dataset, 
-              'features': f, 
-              'z': df_.loc[f],
-              'm': mm, 
-              's': ss} for f, mm, ss in zip(df_m.index, m, s)]
-
-df_m = pd.DataFrame(perf).pivot('features', 'dataset', 'm')
-df_s = pd.DataFrame(perf).pivot('features', 'dataset', 's')
-df_z = pd.DataFrame(perf).pivot('features', 'dataset', 'z')
-
-models = ['motionnet', 'dorsalnet']
-datasets = ['mt1', 'mt2', 'mst']
-
-df_m = df_m[datasets].loc[models]
-df_s = df_s[datasets].loc[models]
-df_z = df_z[datasets].loc[models]
-    
-the_str = f"\\begin{{tabular}}{{{'l' * (df_m.shape[1] + 1)}}}\n\\toprule \n"
-the_str += '{} & ' + ' & '.join(df_m.columns.tolist()).replace('\\', '\\\\').replace('_', '\_') + '\\\\\n'
-the_str += '\midrule \n'
-
-for idx, row in df_m.iterrows():
-    vals = [idx.replace('_', '\_')]
-    for feature, v in row.items():
-        if np.isnan(v):
-            vals.append('-')
-        else:
-            if df_z.loc[idx, feature].item() < -1.96:
-                vals.append(f"{v:.3f} ({df_s.loc[idx, feature]:.3f})"[1:])
-            else:
-                vals.append(f"\\textbf{{{v:.3f}}} ({df_s.loc[idx, feature]:.3f})")
-                
-    the_str += ' & '.join(vals) + '\\\\\n'
-
-the_str += "\end{tabular}\n"
-print(the_str)
-
-# %%
-import scipy.stats
-zvals = pd.DataFrame(2*scipy.stats.norm.cdf(df_z))
-zvals.columns = df_z.columns
-zvals.index = df_z.index
-zvals
-
-# %%
-datasets = df.dataset.unique()
-perf = []
-for dataset in datasets:
-    df_ = df.query(f'dataset == "{dataset}"')
-    df_m = df_.pivot_table('rnorm', 'features', 'layer')
-    best_layer = df_m.idxmax(axis=1)
-
-    good_combos = [f'{x}_{y:02}' for x, y in zip(best_layer.index, best_layer.values)]
-    df_ = df_[df_.features_layer.isin(good_combos)]
-    df_ = df_.pivot_table('rnorm', 'subset', 'features')
-    plt.figure()
-    g = sns.scatterplot(x='motionnet', y='dorsalnet', data=df_, ax=plt.gca())
-    
-    g.plot([-1, 1], [-1, 1], 'k-')
-    plt.axis('square')
-    g.set_xlim([-.05, .8])
-    g.set_ylim([-.05, .8])
-    plt.title(dataset)
-    
-    nbetter = (df_.dorsalnet > df_.motionnet).sum()
-    ntotal = df_.shape[0]
-    print(f"{dataset}, cells better {nbetter}/{ntotal}")
-    
-
-# %% [markdown]
-# # Present CKA results
-
-# %%
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-A = np.load('../cka.npy')
-plt.figure(figsize=(5, 4))
-f = sns.heatmap(A[:-1, :-1], annot=True)
-plt.xlabel('layer')
-plt.ylabel('layer')
-plt.title('CKA across layers of DorsalNet')
-plt.ylim([0, 6])
-plt.savefig('../revision/cka.pdf')
 
 # %% [markdown] tags=[]
 # # Bump plots
-# # Calculate bump plots for alignment between area and layer across all relative scalings of the data
-
-# %%
-import collections
-import numpy as np
-import pickle
-import torch
-from tqdm import tqdm
-import wandb
-
-import sys
-sys.path.append('../')
-import models
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from python_dict_wrapper import wrap
-import torch
-import seaborn as sns
-
-import matplotlib
-
-matplotlib.rcParams["font.family"] = "Arial"
-matplotlib.rcParams['font.sans-serif'] = "Arial"
-sns.set(font="Arial", style="ticks")
+# Calculate bump plots for alignment between area and layer across all relative scalings of the data
 
 # %%
 api = wandb.Api()
@@ -631,7 +302,6 @@ runs = api.runs("pmin/train_fmri_convex.py",
                )
 
 archives = {}
-
 
 print("Found %i" % len(runs))
 for run in tqdm(runs):
@@ -697,10 +367,8 @@ df = df.sort_values('key')
 nums = df.groupby(['dataset', 'features', 'layer', 'scale']).subset.count().reset_index()
 df = df.merge(nums, left_on=('dataset', 'features', 'layer', 'scale'), right_on=('dataset', 'features', 'layer', 'scale'), suffixes=('', '_overall'))
 df = df[df.subset_overall.isin([23, 25, 36, 44, 84])]
-#df.query('dataset == "mst"').groupby('features_layer').mean()
-#df.query('features == "dorsalnet"')
-nums
 
+# %%
 
 for ratio in [0.66, 1.0, 1.5]:
     def bs(df):
@@ -721,13 +389,6 @@ for ratio in [0.66, 1.0, 1.5]:
     A = np.array([bs(df_).values for x in range(500)])
     df__ = pd.DataFrame(A.T)
     df__.index = bs(df_).index
-    plt.figure(figsize=(4, 3))
-    #g = sns.lineplot(x='layer', 
-    #                y='rnorm_max', 
-    #                hue='area',
-    #                palette="Set2",
-    #                data=df_, legend=False, ax=plt.gca())
-    #np.quantile(A, [.025, .0975], axis=0)
     plt.figure(figsize=(4, 3))
 
     lines = []
@@ -764,12 +425,10 @@ for ratio in [0.66, 1.0, 1.5]:
     plt.ylim([0, 1.05])
     plt.title(f'Scaled {ratio}X')
     
-    plt.savefig(f'/home/pmin/paper-assets/figure-2-layer-alignment-rescaled-{ratio}.pdf', bbox_inches='tight')
-    plt.savefig(f'/home/pmin/paper-assets/figure-2-layer-alignment-rescaled-{ratio}.png', bbox_inches='tight')
+    #plt.savefig(f'/home/pmin/paper-assets/figure-2-layer-alignment-rescaled-{ratio}.pdf', bbox_inches='tight')
+    #plt.savefig(f'/home/pmin/paper-assets/figure-2-layer-alignment-rescaled-{ratio}.png', bbox_inches='tight')
 
 # %%
-#df_ = df.groupby(['dataset', 'features', 'layer']).rnorm.agg(['mean', 'sem', 'count'])
-#df_.reset_index().groupby(['dataset', 'features'])['mean'].max()
 datasets = df.dataset.unique()
 
 for ratio in [0.66, 1.0, 1.5]:
@@ -914,13 +573,8 @@ df = df.sort_values('key')
 nums = df.groupby(['dataset', 'features', 'layer']).subset.count().reset_index()
 df = df.merge(nums, left_on=('dataset', 'features', 'layer'), right_on=('dataset', 'features', 'layer'), suffixes=('', '_overall'))
 df = df[df.subset_overall.isin([23, 25, 36, 44, 84])]
-#df.query('dataset == "mst"').groupby('features_layer').mean()
-#df.query('features == "dorsalnet"')
-nums
 
 # %%
-#df_ = df.groupby(['dataset', 'features', 'layer']).rnorm.agg(['mean', 'sem', 'count'])
-#df_.reset_index().groupby(['dataset', 'features'])['mean'].max()
 datasets = df.dataset.unique()
 datasets = ['pvc1', 'pvc4', 'mt1', 'mt2', 'mst']
 df_ms = []
@@ -955,12 +609,9 @@ df_m = pd.DataFrame(perf).pivot('features', 'dataset', 'm')
 df_s = pd.DataFrame(perf).pivot('features', 'dataset', 's')
 df_z = pd.DataFrame(perf).pivot('features', 'dataset', 'z')
 
-#models = ['motionnet', 'dorsalnet']
-#datasets = ['pvc1', 'pvc4', 'mt1', 'mt2', 'mst']
-
-df_m = df_m[datasets]#.loc[models]
-df_s = df_s[datasets]#.loc[models]
-df_z = df_z[datasets]#.loc[models]
+df_m = df_m[datasets]
+df_s = df_s[datasets]
+df_z = df_z[datasets]
 
 # %%
 the_str = f"\\begin{{tabular}}{{{'l' * (df_m.shape[1] + 2)}}}\n\\toprule \n"
@@ -981,11 +632,10 @@ for idx, row in df_m.iterrows():
     the_str += ' & '.join([x.replace('0.', '.') for x in vals]) + '\\\\\n'
 
 the_str += "\end{tabular}\n"
-#print(f"Ratio: {ratio}")
 print(the_str)
 
 # %% [markdown]
-# # Resizing, with boosting
+# # Boosting results with resizing
 
 # %%
 api = wandb.Api()
@@ -1138,13 +788,12 @@ for idx, row in df_m.iterrows():
     the_str += ' & '.join([x.replace('0.', '.') for x in vals]) + '\\\\\n'
 
 the_str += "\end{tabular}\n"
-#print(f"Ratio: {ratio}")
 print(the_str)
 
 # %%
 
 # %% [markdown]
-# # Resizing, with boosting
+# # Resizing, boosting with downsampling
 
 # %%
 api = wandb.Api()
@@ -1191,8 +840,6 @@ for run in tqdm(runs):
                                 }
 
 # %%
-
-
 results = []
 for k, v in archives.items():
     fam = families[v['config']['features']]
@@ -1230,13 +877,9 @@ df = df.sort_values('key')
 nums = df.groupby(['dataset', 'features', 'layer']).subset.count().reset_index()
 df = df.merge(nums, left_on=('dataset', 'features', 'layer'), right_on=('dataset', 'features', 'layer'), suffixes=('', '_overall'))
 df = df[df.subset_overall.isin([23, 25, 36, 44, 84])]
-#df.query('dataset == "mst"').groupby('features_layer').mean()
-#df.query('features == "dorsalnet"')
 nums
 
 # %%
-#df_ = df.groupby(['dataset', 'features', 'layer']).rnorm.agg(['mean', 'sem', 'count'])
-#df_.reset_index().groupby(['dataset', 'features'])['mean'].max()
 datasets = df.dataset.unique()
 datasets = ['mt1', 'mt2', 'mst']
 df_ms = []
@@ -1271,12 +914,10 @@ df_m = pd.DataFrame(perf).pivot('features', 'dataset', 'm')
 df_s = pd.DataFrame(perf).pivot('features', 'dataset', 's')
 df_z = pd.DataFrame(perf).pivot('features', 'dataset', 'z')
 
-#models = ['motionnet', 'dorsalnet']
-#datasets = ['pvc1', 'pvc4', 'mt1', 'mt2', 'mst']
 
-df_m = df_m[datasets]#.loc[models]
-df_s = df_s[datasets]#.loc[models]
-df_z = df_z[datasets]#.loc[models]
+df_m = df_m[datasets]
+df_s = df_s[datasets]
+df_z = df_z[datasets]
 
 # %%
 the_str = f"\\begin{{tabular}}{{{'l' * (df_m.shape[1] + 2)}}}\n\\toprule \n"
@@ -1297,14 +938,10 @@ for idx, row in df_m.iterrows():
     the_str += ' & '.join([x.replace('0.', '.') for x in vals]) + '\\\\\n'
 
 the_str += "\end{tabular}\n"
-#print(f"Ratio: {ratio}")
 print(the_str)
 
-# %%
-df.query('dataset == "mt2"').groupby(['features', 'layer', 'scale']).first()
-
-# %%
-# Boosting results, version 2
+# %% [markdown]
+# # Boosting results, subsampling
 
 # %%
 api = wandb.Api()
@@ -1389,15 +1026,9 @@ df = pd.DataFrame(results)
 df = df.sort_values('key')
 nums = df.groupby(['dataset', 'features', 'layer']).subset.count().reset_index()
 df = df.merge(nums, left_on=('dataset', 'features', 'layer'), right_on=('dataset', 'features', 'layer'), suffixes=('', '_overall'))
-#df = df[df.subset_overall.isin([23, 25, 36, 44, 84])]
-#df.query('dataset == "mst"').groupby('features_layer').mean()
-#df.query('features == "dorsalnet"')
-#nums.query('dataset == "mst"')
-#df.query('dataset == "mt2" and layer == 1 and features == "motionnet" and scale == 1.5')
+df = df[df.subset_overall.isin([23, 25, 36, 44, 84])]
 
 # %%
-#df_ = df.groupby(['dataset', 'features', 'layer']).rnorm.agg(['mean', 'sem', 'count'])
-#df_.reset_index().groupby(['dataset', 'features'])['mean'].max()
 datasets = df.dataset.unique()
 datasets = ['pvc1', 'pvc4', 'mt1', 'mt2', 'mst']
 df_ms = []
@@ -1432,15 +1063,9 @@ df_m = pd.DataFrame(perf).pivot('features', 'dataset', 'm')
 df_s = pd.DataFrame(perf).pivot('features', 'dataset', 's')
 df_z = pd.DataFrame(perf).pivot('features', 'dataset', 'z')
 
-#models = ['motionnet', 'dorsalnet']
-#datasets = ['pvc1', 'pvc4', 'mt1', 'mt2', 'mst']
-
-df_m = df_m[datasets]#.loc[models]
-df_s = df_s[datasets]#.loc[models]
-df_z = df_z[datasets]#.loc[models]
-
-# %%
-df_m
+df_m = df_m[datasets]
+df_s = df_s[datasets]
+df_z = df_z[datasets]
 
 # %%
 the_str = f"\\begin{{tabular}}{{{'l' * (df_m.shape[1] + 2)}}}\n\\toprule \n"
@@ -1461,5 +1086,17 @@ for idx, row in df_m.iterrows():
     the_str += ' & '.join([x.replace('0.', '.') for x in vals]) + '\\\\\n'
 
 the_str += "\end{tabular}\n"
-#print(f"Ratio: {ratio}")
 print(the_str)
+
+# %% [markdown]
+# # Present CKA results
+
+# %%
+A = np.load('../cka.npy')
+plt.figure(figsize=(5, 4))
+f = sns.heatmap(A[:-1, :-1], annot=True)
+plt.xlabel('layer')
+plt.ylabel('layer')
+plt.title('CKA across layers of DorsalNet')
+plt.ylim([0, 6])
+#plt.savefig('../revision/cka.pdf')
---
+++ notebooks/Show Optimal Stimuli.py
@@ -0,0 +1,158 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,md:myst,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# Calculate prefered stimuli at each layer using gradient descent with a patched Lucent.
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
+
+# %%
+import sys
+sys.path.append('../')
+from paths import *
+sys.path.append("../../CPC/dpc")
+sys.path.append("../../CPC/backbone")
+
+from python_dict_wrapper import wrap
+
+features = 'airsim_04'
+
+args = wrap({'features': features,
+             'ckpt_root': CHECKPOINTS,
+             'slowfast_root': '../../slowfast',
+             'ntau': 1,
+             'subsample_layers': False})
+
+from models import get_feature_model
+
+model, hooks, data = get_feature_model(args)
+
+# %%
+model
+
+# %%
+import lucentpatch
+import lucentpatch.transform
+
+from lucentpatch.render import render_vis
+from lucentpatch.objectives import neuron, slow, tv_slow, intensity_preservation
+from lucent.optvis.objectives import channel
+from lucent.optvis import param
+import lucent
+import lucent.optvis
+import lucent.optvis.transform
+
+import numpy as np
+import torch
+
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from IPython.display import HTML
+
+def get_anim(result, size=4, norm=False):
+    fig = plt.figure(figsize=(size, size))
+    ax = plt.gca()
+    im = plt.imshow([[1]])
+    plt.axis('off')
+    plt.tight_layout()
+    plt.subplots_adjust(left=0.0, right=1.0, top=1.0, bottom=0.0)
+
+    rg = None
+    if norm:
+        rg = [result.min(), result.max()]
+
+    def drawframe(n):
+        if n >= nt:
+            n = nt - 1
+        else:
+            im.set_data(result.transpose((2, 3, 1, 0))[:, :, :, n])
+        if norm:
+            im.set_clim(rg)
+        return (im)
+
+    # blit=True re-draws only the parts that have changed.
+    anim = animation.FuncAnimation(fig, drawframe, frames=nt*2, interval=100, blit=False)
+    plt.close()
+    return anim
+
+npixels = 112
+
+for layer_name, max_filt in [('s1_pool_layer', 64), ('res0_relu', 32), ('res1_relu', 32)]:
+    for filt in range(max_filt):
+        nt = 7
+        device = 'cuda'
+
+        # Most important layers: layer2_1_relu, layer3_0_relu, layer3_1_relu
+        obj = neuron(layer_name, filt, (1, 1)) #+ 2 * slow('input') + 2 * tv_slow('input') #+ 5 * intensity_preservation('input', 112 // 4, 112)
+
+        # sd = None converges faster, but some filters are 0.
+        for sd in [None, .1, .15]:
+            batch_param_f = lambda: param.image(npixels, batch=nt, fft=False, sd=sd)
+            model = model.to(device).eval()
+            result = render_vis(model, 
+                                obj, 
+                                batch_param_f, 
+                                show_image=False, 
+                                show_inline=False, 
+                                thresholds=(512,), 
+                                fixed_image_size=True,
+                                preprocess=False,
+                                transforms=[lucentpatch.transform.normalize(),
+                                            lucent.optvis.transform.pad(12, mode="constant", constant_value=0.5),
+                                            lucent.optvis.transform.jitter(8),
+                                            lucent.optvis.transform.random_scale([1 + (i - 5) / 50.0 for i in range(11)]),
+                                            lucent.optvis.transform.random_rotate(list(range(-10, 11)) + 5 * [0]),
+                                            lucent.optvis.transform.jitter(4),
+                                            #lucentpatch.transform.loop(3)
+                                            ],
+                                )
+
+            if result[0].std() > .01 or sd == .15: 
+                anim = get_anim(result[0], npixels / 72)
+                anim.save(f'figures/{features}_{layer_name}_{filt}.gif', writer='imagemagick', fps=12, bitrate=10000)
+                plt.imsave(f'figures/{features}_{layer_name}_{filt}.png', result[0][4, :, :, :].transpose((1, 2, 0)))
+                break
+
+# %% [markdown]
+# Generate quiver plots from reverse correlation stimuli.
+
+# %% tags=[]
+import pickle
+with open('rc_big.pkl', 'rb') as f:
+    data = pickle.load(f)
+
+# %%
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+features = 'airsim_04'
+
+for layer_name, max_filt in [('layer01', 64), ('layer02', 32), ('layer03', 32)]:
+    for filt in range(max_filt):
+        xx = data[layer_name][filt, :, :, 0]
+        yy = data[layer_name][filt, :, :, 1]
+        maxa = 10*np.sqrt(xx ** 2 + yy ** 2).max()
+        plt.figure(figsize=(2.25, 2.25))
+        plt.quiver(xx, yy, scale=maxa, width=.01)
+        plt.xlim([10.5, 16.5])
+        plt.ylim([16.5, 10.5])
+        plt.box(False)
+        plt.axis(False)
+        plt.savefig(f'/home/pmin/paper-assets/tuningcurves/{features}_{layer_name}_{filt}_quiver.svg')
+        plt.savefig(f'/home/pmin/paper-assets/tuningcurves/{features}_{layer_name}_{filt}_quiver.png')
+        plt.show()
