31658731c203e339a9ab03f2a2ea0b6cdcbb1460
Patrick Mineault <patrick.mineault@gmail.com>
2022-01-25 23:45:05 -0500

    max R2 code

--- scripts/derive_maxr2.py
+++ scripts/derive_maxr2.py
@@ -2,17 +2,30 @@ import numpy as np
 import sys
 
 sys.path.append("../")
-# sys.path.append("../loaders/")
 
-from derive_dataset import get_max_r2, get_max_r2_alt
 from loaders import pvc1
+
+
+def get_max_r2(Yall_st):
+    # From Sahani & Linden (2003), How Linear are Auditory Cortical Responses?
+    assert Yall_st.shape[0] < Yall_st.shape[1]
+    # Equation 1
+    signal_power = (
+        1
+        / (Yall_st.shape[0] - 1)
+        * (Yall_st.shape[0] * Yall_st.mean(0).var() - Yall_st.var(1).mean())
+    )
+    response_power = Yall_st.mean(0).var()
+    max_r2 = signal_power / response_power
+    return max_r2
+
 
 if __name__ == "__main__":
     """Only for pvc1. See generate_hyperflow for the method for HyperFlow."""
     maxr2s = []
     for single_cell in range(23):
         loader = pvc1.PVC1(
-            "/mnt/e/data_derived/crcns-ringach-data/",
+            "/mnt/e/data_derived/crcns-pvc1/",
             nt=1,
             ntau=10,
             nframedelay=0,
@@ -25,12 +38,10 @@ if __name__ == "__main__":
             Ys.append(Y)
 
         Y = np.concatenate(Ys, axis=0)
-        Y = Y.reshape((1, Y.shape[0], Y.shape[1])).transpose((0, 2, 1))
 
-        maxr2 = get_max_r2(Y)
-        maxr2_alt = get_max_r2_alt(Y)
+        maxr2 = get_max_r2(Y.T)
 
-        print(single_cell, maxr2, maxr2_alt)
-        maxr2s.append(maxr2_alt)
+        print(single_cell, maxr2)
+        maxr2s.append(maxr2)
 
     print(maxr2s)
